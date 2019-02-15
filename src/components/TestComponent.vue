f0d682d00fb3b8f29004bf8f7e8a883caefed63d
hayden7913 <hayden321@gmail.com>
2019-02-14 19:30:06 -0800

    first commit

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="test-wrapper">
+    <HeaterControlPanel />
+  </div>
+</template>
+
+<script>
+import NoUiSlider from './NoUiSlider';
+import HeaterControlPanel from './HeaterControlPanel';
+
+export default {
+  name: 'Test',
+  components: {
+    NoUiSlider,
+    HeaterControlPanel,
+  },
+};
+
+</script>
+
+<style>
+.test-wrapper {
+  padding-left: 20%;
+  margin-top: 50px;
+  width: 600px;
+}
+
+.test-card {
+  padding: 20px;
+}
+</style>
